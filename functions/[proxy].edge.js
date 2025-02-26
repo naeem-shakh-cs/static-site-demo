0b17dafa3f23b2e69849d68d6c41749f93533886
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-02-26 12:02:15 +0530

    delete header

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,4 +1,4 @@
 export default function handler(request, context) {
-    request.headers.set("x-launch-deploymentuid", "67beb0ee96a15fe79f602a72");
+    request.headers.delete("x-launch-deploymentuid");
     return fetch(request)
 }
