5806bc19164fbfac2a880ee39b734134d1af3e24
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2025-03-10 12:55:04 +0530

    Update [name].js

--- functions/user/[name].js
+++ functions/user/[name].js
@@ -1,6 +1,3 @@
-export default function handler(request, response) {
-  console.log(`Running on Node.js version: ${process.version}`);
-
-  const name = request.query.name || "Guest"; // Use query parameter or default
-  response.status(200).send(`Hello ${name}`);
-}
+export default function handler(request, response){
+    response.status(200).send(`Hello ${request.params.name}`);
+  }
